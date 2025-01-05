006acb872ddb417de08fbe7ca50a44f42f190a48
Jackbear8868 <leo1314520528868@gmail.com>
2025-01-05 11:04:27 +0800

    ERC721

--- backend/src/DMartERC721.sol
+++ backend/src/DMartERC721.sol
@@ -1,158 +1,39 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import './interfaces/IDMartERC721.sol';
+import 'openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 
-contract DMartERC721 is IDMartERC721 {
+contract DMartERC721 is ERC721URIStorage {
+    address internal owner;
+    uint256 id;
 
-    string public constant name = "DMart";
-    string public constant symbol = "DMNFT";
+    error NotOwner();
 
-    mapping(uint256 => address) private _owners;
-    mapping(address => uint256) private _balances;
-    mapping(uint256 => address) private _tokenApprovals;
-    mapping(address => mapping(address => bool)) private _operatorApprovals;
+    constructor( string memory name_, string memory symbol_) payable ERC721(name_, symbol_) {
+        owner = msg.sender;
+    }
 
-    bytes32 public DOMAIN_SEPARATOR;
-    bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
-    mapping(uint256 => uint256) public nonces;
-
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
-
-    constructor() {
-        uint256 chainId;
-        assembly {
-            chainId := chainid()
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert NotOwner();
         }
-        DOMAIN_SEPARATOR = keccak256(
-            abi.encode(
-                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-                keccak256(bytes(name)),
-                keccak256(bytes("1")),
-                chainId,
-                address(this)
-            )
-        );
+        _;
     }
 
-    function balanceOf(address owner) external view override returns (uint256) {
-        require(owner != address(0), "DMartERC721: address zero is not a valid owner");
-        return _balances[owner];
+    function mint(address to, string memory uri) external onlyOwner returns (uint256) {
+        id += 1;
+        uint256 newId = id;
+
+        _safeMint(to, newId);
+        _setTokenURI(newId, uri); // Set the metadata URI
+        return id;
     }
 
-    function ownerOf(uint256 tokenId) external view override returns (address) {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "DMartERC721: invalid token ID");
-        return owner;
+    function _baseURI() internal pure override returns (string memory) {
+       return "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
     }
 
-    function approve(address to, uint256 tokenId) external override {
-        address owner = _owners[tokenId];
-        require(to != owner, "DMartERC721: approval to current owner");
-        require(msg.sender == owner || isApprovedForAll(owner, msg.sender), "DMartERC721: not authorized");
-        _tokenApprovals[tokenId] = to;
-        emit Approval(owner, to, tokenId);
-    }
-
-    function getApproved(uint256 tokenId) public view override returns (address) {
-        require(_owners[tokenId] != address(0), "DMartERC721: invalid token ID");
-        return _tokenApprovals[tokenId];
-    }
-
-    function setApprovalForAll(address operator, bool approved) external override {
-        _operatorApprovals[msg.sender][operator] = approved;
-        emit ApprovalForAll(msg.sender, operator, approved);
-    }
-
-    function isApprovedForAll(address owner, address operator) public view override returns (bool) {
-        return _operatorApprovals[owner][operator];
-    }
-
-    function transferFrom(address from, address to, uint256 tokenId) external override {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "DMartERC721: not authorized");
-        _transfer(from, to, tokenId);
-    }
-
-    function safeTransferFrom(address from, address to, uint256 tokenId) external override {
-        safeTransferFrom(from, to, tokenId, "");
-    }
-
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override {
-        require(_isApprovedOrOwner(msg.sender, tokenId), "DMartERC721: not authorized");
-        _safeTransfer(from, to, tokenId, data);
-    }
-
-    function _mint(address to, uint256 tokenId) internal {
-        require(to != address(0), "DMartERC721: mint to the zero address");
-        require(_owners[tokenId] == address(0), "DMartERC721: token already minted");
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-        emit Transfer(address(0), to, tokenId);
-    }
-
-    function _burn(uint256 tokenId) internal {
-        address owner = _owners[tokenId];
-        _balances[owner] -= 1;
-        delete _owners[tokenId];
-        delete _tokenApprovals[tokenId];
-        emit Transfer(owner, address(0), tokenId);
-    }
-
-    function _transfer(address from, address to, uint256 tokenId) internal {
-        require(_owners[tokenId] == from, "DMartERC721: not the owner");
-        require(to != address(0), "DMartERC721: transfer to the zero address");
-
-        _balances[from] -= 1;
-        _balances[to] += 1;
-        _owners[tokenId] = to;
-
-        delete _tokenApprovals[tokenId];
-        emit Transfer(from, to, tokenId);
-    }
-
-    function _safeTransfer(address from, address to, uint256 tokenId, bytes memory data) internal {
-        _transfer(from, to, tokenId);
-        require(_checkOnERC721Received(from, to, tokenId, data), "DMartERC721: transfer to non ERC721Receiver implementer");
-    }
-
-    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory data) private returns (bool) {
-        if (to.isContract()) {
-            try IERC721Receiver(to).onERC721Received(msg.sender, from, tokenId, data) returns (bytes4 retval) {
-                return retval == IERC721Receiver.onERC721Received.selector;
-            } catch {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    function permit(
-        address owner,
-        address spender,
-        uint256 tokenId,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external {
-        require(block.timestamp <= deadline, "DMartERC721: EXPIRED");
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, tokenId, nonces[tokenId]++, deadline))
-            )
-        );
-        address recoveredAddress = ecrecover(digest, v, r, s);
-        require(recoveredAddress != address(0) && recoveredAddress == owner, "DMartERC721: INVALID_SIGNATURE");
-        _tokenApprovals[tokenId] = spender;
-        emit Approval(owner, spender, tokenId);
-    }
-
-    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
-        address owner = _owners[tokenId];
-        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+    function totalSupply() public view returns (uint256) {
+        return id;
     }
 }
